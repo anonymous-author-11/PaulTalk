12c83691018bd0d3309c6ce8c8b7362e5d0161e0
PaulKeenan11 <117539674+anonymous-author-11@users.noreply.github.com>
2025-02-02 12:45:53 -0500

    deleted qat

--- debug/qat-main/qir/qir-tests/conftest.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-#
-from comparison_tests import *  # noqa: F403
-from reduction_tests import *  # noqa: F403
-from scenario_tests import *  # noqa: F403
-from validation_tests import *  # noqa: F403
-
-__all__ = all_comparison + all_reduction + all_validation + all_scenarios  # noqa: F405
