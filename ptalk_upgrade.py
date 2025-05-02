12882b68091928ca9576e3ea26bed8e98999500d
PaulKeenan11 <117539674+anonymous-author-11@users.noreply.github.com>
2025-05-02 12:08:28 -0400

    Prepare for release

--- ptalk_upgrade.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-
-def upgrade_main(argv):
-	command = ["0install","update","https://raw.githubusercontent.com/anonymous-author-11/PaulTalk/refs/heads/main/ptalk.xml"]
-	subprocess.run(command)
-
-if __name__ == "__main__":
-	upgrade_main(sys.argv)
