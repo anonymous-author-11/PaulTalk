10ef4d20a4b1971b21c83445c7f649edc4cd274b
PaulKeenan11 <117539674+anonymous-author-11@users.noreply.github.com>
2025-06-01 15:40:14 -0400

    Prepare for release

--- xdsl/utils/test_value.py
+++
@@ -1,7 +0,0 @@
-from xdsl.ir import Block, Operation, SSAValue
-
-
-class TestSSAValue(SSAValue):
-    @property
-    def owner(self) -> Operation | Block:
-        assert False, "Attempting to get the owner of a `TestSSAValue`"
